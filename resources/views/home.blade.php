e136291fc2d54b77d73cc934dddf72c14dfe0d69
Rohit Khandelwal <rohit.khandelwal.2059@gmail.com>
2026-03-12 14:53:58 +0530

    download method routing done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="container mt-5">
+
+    <h1 class="text-primary">Welcome to Laravel with Bootstrap</h1>
+
+    <button class="btn btn-success">Click Me</button>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user/{name}', function ($name) {
+    return "Hello " . $name;
+});
 Route::get('/about', function () {
     return "This is the About Page";
+});
+
+
+Route::get('/home', function () {
+    return view('home') ;
+});
+
+
+/// defaut parameter
+Route::get('/user/{name?}', function ($name = "Guest") {
+    return "Hello " . $name;
+});
+
+
+/// add two numbers
+Route::get('/add/{num1}/{num2}', function ($num1, $num2) {
+    return $num1 + $num2;
+});
+Route::get('/download', function () {
+    return response()->download(public_path('files/sample.pdf'));
 });
